e745b63c7c60fe0873b63a3b9f41adf9225d50d3
DELL <1611323711@qq.com>
2024-05-24 20:47:19 +0800

    Initial commit

---
+++ src/main/scala/com/wangle/HelloWorld.scala
@@ -0,0 +1,16 @@
+package com.wangle
+
+/**
+ * @Projectname: scala_learnning
+ * @Filename: com.wangle.HelloWorld
+ * @Author: Wang Le
+ * @Data:2024/5/23 22:22
+ * @Description: TODO
+ *
+ */
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+  }
+}
